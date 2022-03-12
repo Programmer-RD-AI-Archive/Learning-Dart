2754e958fc59a0acfe6f1b1aedff3e7af5e8c8d6
Programmer-RD-AI <go2ranuga@gmail.com>
2022-03-12 20:04:32 +0530

    variables.dart history
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Basics/variables_20220311212302.dart
@@ -0,0 +1,10 @@
+void main(){int age = 70;
+String name = "Mike";
+
+print("There once was a man named ${name}");
+print("He was ${age} years old");
+
+age = 25
+
+print("He really liked the name ${name}");
+print("But didn't like being ${age}");}
